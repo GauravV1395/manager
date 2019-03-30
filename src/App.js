4669a97caccff8d1e70b70506e67b6c279617568
unknown <gaurav.v@appliedsni.com>
2019-03-30 12:57:50 +0530

    first message

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import ReduxThunk from "redux-thunk";
+import firebase from "firebase";
+import reducers from "./reducers";
+import LoginForm from "./components/LoginForm";
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyDwaP0jctDtilYAyfHDiccf--dzTxfhnPA",
+      authDomain: "manager-d43a1.firebaseapp.com",
+      databaseURL: "https://manager-d43a1.firebaseio.com",
+      projectId: "manager-d43a1",
+      storageBucket: "manager-d43a1.appspot.com",
+      messagingSenderId: "1023606467081"
+    };
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+    return (
+      <Provider store={store}>
+        <LoginForm />
+      </Provider>
+    );
+  }
+}
+
+export default App;
