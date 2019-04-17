b11e8e055a0ec419d51a71044de328004c59b478
unknown <gaurav.v@appliedsni.com>
2019-04-17 17:40:35 +0530

    alert modal has been added

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import _ from "lodash";
-import { Card, CardSection, Button } from "./common";
+import { Card, CardSection, Button, Confirm } from "./common";
 import { connect } from "react-redux";
 import EmployeeForm from "./EmployeeForm";
 import Communications from "react-native-communications";
 import { employeeUpdate, employeeSave } from "../actions";
 
 class EmployeeEdit extends Component {
+  state = { showModal: false };
   componentWillMount() {
     _.each(this.props.employee, (value, prop) => {
       this.props.employeeUpdate({ prop, value });
@@ -32,6 +33,20 @@ class EmployeeEdit extends Component {
     );
   }
 
+  onFirePress() {
+    this.setState({
+      showModal: !this.state.showModal
+    });
+  }
+
+  onAccept() {}
+
+  onDecline() {
+    this.setState({
+      showModal: false
+    });
+  }
+
   render() {
     return (
       <Card>
@@ -42,6 +57,17 @@ class EmployeeEdit extends Component {
         <CardSection>
           <Button onPress={this.onTextPress.bind(this)} children="Text" />
         </CardSection>
+        <CardSection>
+          <Button onPress={this.onFirePress.bind(this)} children="Fire" />
+        </CardSection>
+
+        <Confirm
+          visible={this.state.showModal}
+          onAccept={onAccept.bind(this)}
+          onDecline={onDecline.bind(this)}
+        >
+          Are you sure you want to delete this?
+        </Confirm>
       </Card>
     );
   }
